5f857da1fc0547466997d5f53310db4863f1938a
stupns <stupns@i.ua>
2017-02-17 15:10:57 +0200

    initial commit

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+#test 123
 class Product(models.Model):
     name = models.CharField(max_length=64, blank=True, null=True, default=None)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
